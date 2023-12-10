d3038ddefb7b38ab0af17c725f185f63e99c0fa2
UBauddhik <125404632+UBauddhik@users.noreply.github.com>
2023-12-09 22:19:39 -0500

    this is good to check out :)

--- src/tv-app.js
+++ src/tv-app.js
@@ -9,53 +9,85 @@ export class TvApp extends LitElement {
   // defaults
   constructor() {
     super();
-    this.name = '';
     this.source = new URL('../assets/channels.json', import.meta.url).href;
     this.listings = [];
     this.activeIndex = 0;
   }
 
+  /**
+ * Lifecycle callback for when the component is connected to the DOM.
+ * Sets up an interval to periodically check the current time of the video
+ * and update the active channel index accordingly.
+ */
   connectedCallback() {
     super.connectedCallback();
+    // Set an interval to check the video's current time every second
     this._updateTimeInterval = setInterval(() => {
+      // Access the video player element and its current playback time
       const videoPlayer = this.shadowRoot.querySelector('video-player').shadowRoot.querySelector('a11y-media-player');
       const currentTime = videoPlayer.currentTime;
-      console.log(`[Interval Callback] currentTime: ${currentTime}`);
+      
+      // Find the active index based on the current time of the video
       let newActiveIndex = this.findActiveIndex(currentTime);
-      console.log(`[Interval Callback] newActiveIndex: ${newActiveIndex}, current activeIndex: ${this.activeIndex}`);
+      
+      // Update the active index if it's different from the current activeIndex
       if (newActiveIndex !== this.activeIndex) {
           this.activeIndex = newActiveIndex;
-          console.log(`[Interval Callback] Updated activeIndex: ${this.activeIndex}`);
-          this.requestUpdate(); // Trigger an update to reflect changes
+          // Request an update to re-render the component with the new active index
+          this.requestUpdate(); 
       }
-  }, 1000);}
-
-findActiveIndex(currentTime) {
-  console.log(`[findActiveIndex] currentTime: ${currentTime}`);
-  for (let i = this.listings.length - 1; i >= 0; i--) {
-      if (currentTime >= this.listings[i].metadata.timecode) {
-        console.log(`[findActiveIndex] Returned Index: ${i}`);
-        return i;
-      }
+    }, 1000);
   }
-  return 0; // Default to the first index if no match is found
-}
 
+  /**
+  * Determines the active index (channel) based on the current time of the video.
+  * This function iterates backwards through the listings array to find the first
+  * listing whose timecode is less than or equal to the given currentTime.
+  * @param {number} currentTime - The current time of the video player.
+  * @returns {number} The index of the active channel based on the current time.
+  */
+
+  findActiveIndex(currentTime) {
+    // Iterate backwards through the listings
+    for (let i = this.listings.length - 1; i >= 0; i--) {
+        // If the current time is greater than or equal to a listing's timecode,
+        // return that listing's index as the active index
+        if (currentTime >= this.listings[i].metadata.timecode) {
+            return i;
+        }
+    }
+    // If no match is found, return 0 (default to the first index)
+    return 0;
+  }
+
+
+  /**
+  * Lifecycle callback for when the component is disconnected from the DOM.
+  * Clears the interval set in connectedCallback to prevent memory leaks.
+  */
 
   disconnectedCallback() {
     super.disconnectedCallback();
+    // Clear the interval to stop updating the active index
     if (this._updateTimeInterval) {
       clearInterval(this._updateTimeInterval);
     }
   }
 
-  
-  // Lifecycle callback for when the element is added to the DOM
+  /**
+  * Updates the currently active slide based on a new index.
+  * Optionally updates the video time to match the new slide.
+  * @param {number} newIndex - The new index to set as the active slide.
+  * @param {boolean} [updateVideo=true] - Flag to control whether to update the video time.
+  */
+
   updateSlide(newIndex, updateVideo = true) {
-    console.log(`[updateSlide] newIndex: ${newIndex}, current activeIndex: ${this.activeIndex}`);
+    // Check if the newIndex is valid and different from the current activeIndex
     if (newIndex >= 0 && newIndex < this.listings.length && newIndex !== this.activeIndex) {
+      // Update the activeIndex to the new index
       this.activeIndex = newIndex;
-      console.log(`[updateSlide] Updated activeIndex: ${this.activeIndex}`);
+      
+      // If updateVideo flag is true, seek the video to the new slide's timecode
       if (updateVideo) {
         const videoPlayer = this.shadowRoot.querySelector('video-player').shadowRoot.querySelector('a11y-media-player');
         videoPlayer.seek(this.listings[newIndex].metadata.timecode);
@@ -63,14 +95,23 @@ findActiveIndex(currentTime) {
       }
     }
   }
-  
+
+  /**
+  * Navigates to the next slide.
+  */
+
   nextSlide() {
     this.updateSlide(this.activeIndex + 1);
   }
-  
+
+  /**
+  * Navigates to the previous slide.
+  */
+
   previousSlide() {
     this.updateSlide(this.activeIndex - 1);
   }
+
 
   // convention I enjoy using to define the tag's name
   static get tag() {
@@ -97,9 +138,9 @@ findActiveIndex(currentTime) {
         margin: 16px;
         padding: 16px;
         height: calc(100vh - 32px);
-        background-color: #f0f0f0; /* Softer shade for background */
-        color: #333; /* Dark grey for better readability */
-        font-family: 'Roboto', sans-serif; /* Modern, readable font */
+        background-color: #f0f0f0; 
+        color: #333; 
+        font-family: 'Roboto', sans-serif; 
       }
 
       h1, h2, h3 {
@@ -125,7 +166,7 @@ findActiveIndex(currentTime) {
       .button-box, .lecture-sidebar, .lecture-screen {
         background-color: #fff;
         border-radius: 8px;
-        box-shadow: 0 2px 4px rgba(0,0,0,0.1); /* Subtle shadow for depth */
+        box-shadow: 0 2px 4px rgba(0,0,0,0.1); 
       }
 
       .lecture-screen {
@@ -146,13 +187,12 @@ findActiveIndex(currentTime) {
 
       .player-description {
         grid-area: player-description;
-        background-color: #FFFFFF; /* Assuming white fits the theme */
-        color: #333; /* Dark grey for text */
+        background-color: #FFFFFF; 
+        color: #333; 
         padding: 16px;
-        margin: 0; /* Adjust as needed */
-        border-radius: 4px; /* Matching the theme's border radius */
-        box-shadow: 0 2px 4px rgba(0,0,0,0.1); /* Subtle shadow */
-        /* Typography consistent with the theme */
+        margin: 0; 
+        border-radius: 4px; 
+        box-shadow: 0 2px 4px rgba(0,0,0,0.1); 
         font-family: 'Roboto', sans-serif;
       }
 
@@ -161,10 +201,10 @@ findActiveIndex(currentTime) {
         display: flex;
         flex-direction: column;
         gap: 10px;
-        overflow-y: auto; /* Enables vertical scrolling if content overflows */
-        max-height: calc(100vh - 120px); /* Adjusted for viewport height and padding */
+        overflow-y: auto; 
+        max-height: calc(100vh - 120px); 
         padding: 10px;
-        border-radius: 8px; /* Consistent border radius */
+        border-radius: 8px; 
         box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
       }
 
@@ -193,7 +233,7 @@ findActiveIndex(currentTime) {
       }
 
       .prev, .next {
-        background-color: #005792; /* New primary color */
+        background-color: #005792;
         color: white;
         border: none;
         border-radius: 4px;
@@ -221,7 +261,7 @@ findActiveIndex(currentTime) {
 
         .grid-container {
           transition: transform 0.3s ease;
-          grid-template-columns: 1fr; /* Stack sidebar below the main content on small screens */
+          grid-template-columns: 1fr; 
           grid-template-rows: auto 50px auto 1fr;
           grid-template-areas:
             "lecture-screen"
@@ -237,6 +277,7 @@ findActiveIndex(currentTime) {
 
   // LitElement rendering template of your element
   render() {
+    // Determine the active channel based on the current activeIndex.
     const activeChannel = this.activeIndex >= 0 && this.activeIndex < this.listings.length
     ? this.listings[this.activeIndex]
     : null;
@@ -244,90 +285,112 @@ findActiveIndex(currentTime) {
     return html`
       <div class="grid-container">
         
+        <!-- Video Player Section -->
         <div class="lecture-screen">
+          <!-- Embed the video player with the specified source URL -->
           <video-player source = 'https://youtu.be/-HmaAl2X09E'></video-player>
         </div>
 
+        <!-- Navigation Buttons Section -->
         <div class="button-box">
-          <!-- Information about the current slide -->
+          <!-- Button to navigate to the previous slide -->
           <button class="prev" @click="${this.previousSlide}">Previous Slide</button>
+          <!-- Button to navigate to the next slide -->
           <button class="next" @click="${this.nextSlide}">Next Slide</button>
         </div>
 
+        <!-- Player Description Section -->
         <div class="player-description">
+          <!-- Conditionally display the title and description of the active channel -->
           ${activeChannel ? html`
             <h3>${activeChannel.title}</h3>
             <p>${activeChannel.description}</p>
           ` : html`<p>No active slide.</p>`}
         </div>
 
+        <!-- Lecture Sidebar Section -->
         <div class ="lecture-sidebar">
+          <!-- Map through the listings to generate tv-channel elements -->
           ${
             this.listings.map(
               (item, index) => html`
                 <tv-channel
-                ?active="${index === this.activeIndex}" 
-                .index="${index}" 
-                .title="${item.title}"
-                .description="${item.description}"
-                .timecode="${item.metadata.timecode}"
-                .thumbnail="${item.metadata.thumbnail}"
-                @click="${() => { 
-              console.log(`[Click Event] Channel Index: ${index}`);
-              this.updateSlide(index);
-            }}"
-          ></tv-channel>
-        `)}
+                  ?active="${index === this.activeIndex}" 
+                  .index="${index}" 
+                  .title="${item.title}"
+                  .description="${item.description}"
+                  .timecode="${item.metadata.timecode}"
+                  .thumbnail="${item.metadata.thumbnail}"
+                  @click="${() => { 
+                    // Log the channel click event and update the slide
+                    console.log(`[Click Event] Channel Index: ${index}`);
+                    this.updateSlide(index);
+                  }}"
+                ></tv-channel>
+            `)}
         </div> 
       </div>
     `;
-
   }
 
-  // LitElement life cycle for when any property changes | Fetches data from the source URL
+  /**
+   * Lifecycle callback that is invoked after the element’s properties have been updated.
+   * This method ensures that changes to properties trigger appropriate actions.
+   *
+   * @param {Map} changedProperties - A map of properties that have changed, mapping the name to the old value.
+   */
+
   updated(changedProperties) {
     super.updated(changedProperties);
     changedProperties.forEach((oldValue, propName) => {
-        // Handle source changes
-        if (propName === "source" && this[propName]) {
-            this.updateSourceData(this[propName]);
-        }
+      // Handle updates to the 'source' property.
+      if (propName === "source" && this[propName]) {
+        // Update the source data when the 'source' property changes.
+        this.updateSourceData(this[propName]);
+      }
 
-        // Handle activeIndex changes for UI updates
-        if (propName === "activeIndex") {
-            this.updateUI(); // Call the new method to update UI
-        }
+      // Handle updates to the 'activeIndex' property.
+      if (propName === "activeIndex") {
+        // Update the UI to reflect the new active slide.
+        this.updateUI();
+      }
     });
   }
 
-updateUI() {
+  /**
+   * Update the user interface based on the current active index.
+   * This method updates the details displayed for the active slide and ensures the active channel is visible.
+   */
 
-    // Update UI based on activeIndex
+  updateUI() {
+    // Retrieve data for the current active slide.
     const currentSlideData = this.listings[this.activeIndex];
+
     if (currentSlideData) {
-        // Update UI for active slide
-        const activeItem = this.shadowRoot.querySelector(".activeLectureSlide");
-        if (activeItem) {
-            // Update title, presenter, description as necessary
-            activeItem.title = currentSlideData.title;
-            activeItem.presenter = currentSlideData.metadata.author;
-            activeItem.description = currentSlideData.description;
-        }
+      // Locate and update the active item in the UI, if it exists.
+      const activeItem = this.shadowRoot.querySelector(".activeLectureSlide");
+      if (activeItem) {
+        // Populate the active item with title, presenter, and description from the current slide data.
+        activeItem.title = currentSlideData.title;
+        activeItem.presenter = currentSlideData.metadata.author;
+        activeItem.description = currentSlideData.description;
+      }
 
-        // Ensure the active channel is visible in the sidebar
-        const activeChannel = this.shadowRoot.querySelector(`tv-channel[index="${this.activeIndex}"]`);
-        if (activeChannel) {
-            activeChannel.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
-        }
+      // Scroll the active channel into view in the sidebar.
+      const activeChannel = this.shadowRoot.querySelector(`tv-channel[index="${this.activeIndex}"]`);
+      if (activeChannel) {
+        // Smoothly scroll to make the active channel visible.
+        activeChannel.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+      }
 
-        // Update active state of channels
-        const channels = this.shadowRoot.querySelectorAll('tv-channel');
-        channels.forEach((channel, index) => {
-            channel.classList.toggle('active', index === this.activeIndex);
-        });
+      // Update the active state of all channel components in the sidebar.
+      const channels = this.shadowRoot.querySelectorAll('tv-channel');
+      channels.forEach((channel, index) => {
+        // Set the 'active' class based on whether the channel index matches the active index.
+        channel.classList.toggle('active', index === this.activeIndex);
+      });
     }
   }
-
 
   async updateSourceData(source) {
     try {
@@ -339,9 +402,10 @@ updateUI() {
       }
     } catch (error) {
       console.error('Fetch error:', error);
-      // Handle errors appropriately for your application
+      // Handle errors appropriately for the application
     }
   }  
 }
+
 // tell the browser about our tag and class it should run when it sees it
 customElements.define(TvApp.tag, TvApp);
--- src/tv-channel.js
+++ src/tv-channel.js
@@ -27,35 +27,59 @@ export class TvChannel extends LitElement {
     return css`
       :host {
         display: block;
+        margin-bottom: 16px;
       }
   
       .wrapper {
+        position: relative;
         display: flex;
+        flex-direction: column;
         align-items: center;
-        gap: 10px;
-        padding: 10px;
-        margin-bottom: 10px;
-        background-color: #fff;
+        justify-content: center;
+        padding: 20px;
+        background-color: #f8f8f8;
         border: 1px solid #ddd;
         border-radius: 8px;
         box-shadow: 0 2px 4px rgba(0,0,0,0.05);
         cursor: pointer;
         transition: box-shadow 0.3s, background-color 0.3s;
+        overflow: hidden; /* Ensure no overflow outside the border radius */
       }
   
       .wrapper:hover, .wrapper.highlighted {
+        background-color: #e9ecef;
         box-shadow: 0 4px 8px rgba(0,0,0,0.1);
       }
 
       .thumbnail {
-        width: 60px; 
-        height: 60px; 
-        object-fit: cover;
-        border-radius: 4px; 
+        width: 100%; /* Full width of the container */
+        height: 100%;
+        object-fit: contain;
+        
       }
-  
+
+      .text-overlay {
+        position: absolute;
+        bottom: 0; /* Align with the bottom of the container */
+        left: 0;
+        width: 100%;
+        text-align: center;
+        color: #2e2b2b; /* White text for better contrast */
+        padding: 10px;
+        backdrop-filter: blur(15px); /* Apply a blur effect to the background */
+        -webkit-backdrop-filter: blur(15px); /* For Safari browser compatibility */
+      }
+
+
+
+
+      h3, h4 {
+        margin: 0;
+        color: inherit; /* Use color from the parent element */
+      }
+
       .highlighted {
-        background-color: #005792; 
+        background-color: #005792;
         color: #fff;
       }
     `;
@@ -65,11 +89,13 @@ export class TvChannel extends LitElement {
   render() {
     return html`
       <div class="wrapper ${this.highlighted ? 'highlighted' : ''}" >
-      ${this.thumbnail ? html`<img src="${this.thumbnail}" alt="${this.title}" class="thumbnail">` : ''}
-        <h3>${this.title}</h3>
-        <h4>${this.presenter}</h4>
+        ${this.thumbnail ? html`<img src="${this.thumbnail}" alt="${this.title}" class="thumbnail">` : ''}
+        <div class="text-overlay">
+          <h3>${this.title}</h3>
+          <h4>${this.presenter}</h4>
+        </div>
       </div>  
-      `;
+    `;
   }
 }
 // tell the browser about our tag and class it should run when it sees it
